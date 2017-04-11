f1db41a7fc340485c509b8f15002bd568bd722c7
dtaylor31906 <dt01812@georgiasouthern.edu>
2017-04-11 18:33:12 -0400

    logout implemented across all pages

--- Solubuddy/js/login.js
+++ Solubuddy/js/login.js
@@ -1,46 +1,47 @@
 
-$(document).ready(function() {
-    $('login').submit(function(event) {
-
-        // get the form data
-        var formData = {
-            'username': $('input[name=username]').val(),
-            'password': $('input[name=password]').val()};
-
-        // process the form
-        $.ajax({
-            type: 'POST',
-            url: 'php/login.php', // the url where we want POST
-            data: formData, // our data object
-            dataType: 'json', //type data
-		        encode: true
-        })
-            // using the done promise callback
-            .done(function(data) {
-
-                // log data to the console so we can see
-                console.log(data);
-
-                // here we will handle errors and validation messages
-
-                if(data.success) {
-                  document.getElementById('main-login').style.display = "none";
-                  document.getElementById('main-tutorial').style.display = "block";
-                  document.getElementById('main-selections').style.display = "none";
-                } else {
-
-                if(data.errors.username) {
-                  document.getElementById('username').style.borderColor = "#ff0000";
-                }
-
-                if(data.errors.password) {
-                  document.getElementById('password').style.borderColor = "#ff0000";
-                }
-              }
-            });
-
-        // stop the form from submitting the normal way and refreshing the page
-        event.preventDefault();
+$(function () {//onReady
+    initFireBase();
+    initApp();
+    $("#logoutButton").click(function () {
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+            console.log("LOGGED OUT");
+        }).catch(function(error) {
+            // An error happened.
+            console.log("error");
+        });
+    })//click event for logout button.
+})
+//code snippet for any page that uses firebase
+function initFireBase() {
+    var config = {
+        apiKey: "AIzaSyDmWKJArULZoTI8GvpFTagrJ8-48sr7y6k",
+        authDomain: "solubuddy-ffc1c.firebaseapp.com",
+        databaseURL: "https://solubuddy-ffc1c.firebaseio.com",
+        projectId: "solubuddy-ffc1c",
+        storageBucket: "solubuddy-ffc1c.appspot.com",
+        messagingSenderId: "690710200191"
+    };
+    firebase.initializeApp(config);
+}
+//method use to handle authentication. listen for changes in user state
+function initApp() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            console.log(user);
+        } else {
+            // User is signed out.
+            //send user to login
+            window.location.href = 'fireBaseAuth.html';
+        }
+    }, function(error) {
+        console.log(error);
     });
-
-});
+};
