6baf76c87d2210e3dab20d5f1a06281065e0809d
dtaylor31906 <dt01812@georgiasouthern.edu>
2017-04-19 15:02:18 -0400

    Revert "Almost authinticating"

--- Solubuddy/js/login.js
+++ Solubuddy/js/login.js
@@ -1,66 +1,19 @@
-var count = 0;
-var tutorialCount;//helps to make sure tutorial is only showed once per login.
-initFireBase();
-function Login(){
-    tutorialCount = 0;
-    this.auth = firebase.auth();
-    this.database = firebase.database();
-    this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
-}
-Login.prototype.getUser = function () {
-    return this.soluUser;
-}
-Login.prototype.getUid = function () {
-    console.log(this.soluUser.uid+ ""+ count++) ;
-    return this.soluUser.uid;
-}
-function needTutorial (user) {
-    this.checkBoxRef  = firebase.database().ref('users/'+user.uid);
-    this.checkBoxRef.once('value')
-        .then( function(data) {
 
-            console.log(data.val());
-            var dataobject = data.toJSON();
-            console.log(dataobject.doNotShowTutorial);
-            isChecked = data.val().doNotShowTutorial;
-            if(!isChecked)
-            {
-                let location = window.location.href;
-
-                var test = /tutorial/.test(location);
-                if(!test && tutorialCount < 1) {
-                    tutorialCount++;
-                    window.location.href = 'tutorial.html';
-
-                }
-            }
-
-        }, function(error) {
-            console.log(error);
-        });//calls the database
-}
-Login.prototype.onAuthStateChanged = function (user) {
-    if (user) {
-
-        // User is signed in.
-        this.soluUser = user;
-        //tutorial check
-        needTutorial(user);
-        console.log("uid:" + this.soluUser.uid+ count++);
-
-    }
-    else {
-        // User is signed out.
-        //send user to login
-        tutorialCount = 0;
-        window.location.href = 'fireBaseAuth.html';
-    }
-}, function(error) {
-    console.log(error);
-}
+$(function () {//onReady
+    initFireBase();
+    initApp();
+    $("#logoutButton").click(function () {
+        firebase.auth().signOut().then(function() {
+            // Sign-out successful.
+            console.log("LOGGED OUT");
+        }).catch(function(error) {
+            // An error happened.
+            console.log("error");
+        });
+    })//click event for logout button.
+})
 //code snippet for any page that uses firebase
 function initFireBase() {
-
     var config = {
         apiKey: "AIzaSyDmWKJArULZoTI8GvpFTagrJ8-48sr7y6k",
         authDomain: "solubuddy-ffc1c.firebaseapp.com",
@@ -71,5 +24,24 @@ function initFireBase() {
     };
     firebase.initializeApp(config);
 }
-
-
+//method use to handle authentication. listen for changes in user state
+function initApp() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var providerData = user.providerData;
+            console.log(user);
+        } else {
+            // User is signed out.
+            //send user to login
+            window.location.href = 'fireBaseAuth.html';
+        }
+    }, function(error) {
+        console.log(error);
+    });
+};
